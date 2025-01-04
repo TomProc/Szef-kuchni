fdd3ead9e7a9a58b60022abfefaa58d9f99a0727
Tomek <tomek2003gl@gmail.com>
2025-01-04 20:37:04 +0100

    dodane sortowanie

--- backend/app.py
+++ backend/app.py
@@ -17,15 +17,36 @@ import json
 
 
 
+
+#  przykładowe wywołanie http://127.0.0.1:5000/get_recipes?sort_by=time&order=desc
+
 @app.route('/get_recipes', methods=['GET'])
 def get_recipes():
-    recipes = Recipe.query.all()
-    json_recipes = list(map(lambda x: x.to_json(), recipes))
+    
+    sort_by = request.args.get('sort_by', 'id')  # Domyślnie sortowanie po 'id'
+    order = request.args.get('order', 'asc')     # Domyślnie rosnąco (asc)
+
+    
+    #['id', 'name', 'time', 'difficulty'] - Obsługiwane sortowanie sortowania
+
+    
+
+   
+    if order == 'desc':                                  # Pobranie przepisów z bazy danych i sortowanie
+        recipes = Recipe.query.order_by(getattr(Recipe, sort_by).desc()).all()
+    else:
+        recipes = Recipe.query.order_by(getattr(Recipe, sort_by).asc()).all()
+
+    
+    json_recipes = list(map(lambda x: x.to_json(), recipes))# Konwersja wyników do JSON-a
+
+    
     response = app.response_class(
         response=json.dumps({'recipes': json_recipes}, ensure_ascii=False),
         mimetype='application/json; charset=utf-8'
     )
     return response
+
 
 
 if __name__ == '__main__':
