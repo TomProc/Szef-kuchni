df9d9bd9146c7a029945711401ab5d6818b3644f
Tomek <tomek2003gl@gmail.com>
2025-01-07 13:42:32 +0100

    dodane zaznaczanie ulubionych przepisów i wyświetlanie/ zmiana struktury bazy danych

--- backend/models.py
+++ backend/models.py
@@ -5,16 +5,18 @@ class Recipe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     ingredients = db.Column(db.String(255))
-    preparation = db.Column(db.String(255))
     time=db.Column(db.Integer)
     difficulty=db.Column(db.Integer)
+    preparation = db.Column(db.String(255))
+    favourite=db.Column(db.Boolean, default=False)
 
     def to_json(self):
         return {
             'id': self.id,
             'name': self.name,
             'ingredients': self.ingredients,
-            'preparation': self.preparation,
             'time': self.time,
-            'difficulty': self.difficulty
+            'difficulty': self.difficulty,
+            'preparation': self.preparation,
+            'favourite': self.favourite
         }
