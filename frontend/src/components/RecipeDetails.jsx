6690acd8dae89f9125ef969f845091522e111c81
Tomek <tomek2003gl@gmail.com>
2025-01-29 11:53:30 +0100

    component cooking steps- obsługa wyświetlania przepisu krok po kroku

--- frontend/src/components/RecipeDetails.jsx
+++ frontend/src/components/RecipeDetails.jsx
@@ -1,21 +1,25 @@
-// src/components/RecipeDetails.jsx
-// Komponent odpowiedzialny za wyświetlanie szczegółów wybranego przepisu
-import React from "react";
+import React, { useState } from "react";
+import CookingSteps from "./CookingSteps";
 
-// Komponent, który pokazuje szczegóły wybranego przepisu: nazwę, czas, składniki, sposób przygotowania
 const RecipeDetails = ({ recipe, handleClose }) => {
+  const [showCookingSteps, setShowCookingSteps] = useState(false);
+
+  const steps = recipe.preparation.split(",");
+
   return (
     <div className="recipe-details-overlay">
       <div className="recipe-details">
-        {/* Przycisk do zamknięcia szczegółów przepisu */}
         <button className="close-button" onClick={handleClose}>X</button>
-        {/* Wyświetlanie szczegółów przepisu */}
         <h2>{recipe.name}</h2>
         <p><strong>Czas przygotowania:</strong> {recipe.time} minut</p>
         <p><strong>Poziom trudności:</strong> {recipe.difficulty}</p>
         <p><strong>Składniki:</strong> {recipe.ingredients}</p>
         <p><strong>Sposób przygotowania:</strong> {recipe.preparation}</p>
+
+        <button onClick={() => setShowCookingSteps(true)}>Zacznij gotowanie</button>
       </div>
+
+      {showCookingSteps && <CookingSteps steps={steps} onClose={() => setShowCookingSteps(false)} />}
     </div>
   );
 };
